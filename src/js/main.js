7ceb5b43f98f85b7e0a30911d4a9c72f642131a2
vasilievruslan <ruzero.io@gmail.com>
2018-02-08 18:14:56 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+//= slick.js
+
+$(function () {
+	$('.carousel').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		// centerMode: true,
+		// variableWidth: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		responsive: [
+	    {
+	      breakpoint: 824,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2,
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }],
+		prevArrow: $('.prev-arr'),
+		nextArrow: $('.next-arr')
+	});
+
+	AOS.init({
+      offset: 200,
+      duration: 400,
+      easing: 'ease-in-sine',
+      disable: 'mobile'
+    });
+})
